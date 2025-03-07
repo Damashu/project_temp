82de50d301dd2527a4277af6dd138617e25e4c5a
Damashu <2740417462@qq.com>
2025-03-08 03:46:56 +0800

    嘿嘿

--- suep_Guide/main.cpp
+++ suep_Guide/main.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-// i want add a comment
+// i want add a comment heihei 我他妈就要和你们不一样我命由我不由天
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
